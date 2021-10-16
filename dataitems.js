ccbfbaa8be3859d9867f49160a38f28139730d11
rvr007-glitch <79240272+rvr007-glitch@users.noreply.github.com>
2021-10-16 20:29:40 +0530

    Add files via upload

---
+++ dataitems.js
@@ -0,0 +1,58 @@
+export const data = [
+    {
+        name: 'Devlopment Project %',
+
+        per: 5,
+
+    },
+    {
+        name: 'Competative Coding %',
+
+        per: 10,
+
+    },
+    {
+        name: 'Placement %',
+
+        per: 5,
+
+    },
+    {
+        name: 'College Course %',
+
+        per: 55,
+
+    },
+
+
+
+];
+export const javadata = [
+    {
+        name: 'Devlopment Project %',
+
+        per: 25,
+
+    },
+    {
+        name: 'Competative Coding %',
+
+        per: 30,
+
+    },
+    {
+        name: 'Placement %',
+
+        per: 40,
+
+    },
+    {
+        name: 'College Course %',
+
+        per: 25,
+
+    },
+
+
+
+];
---
+++ dev.jsx
@@ -0,0 +1,17 @@
+import Cards from "./components/dev-cards/devCards";
+import React from 'react'
+import "./dev.css"
+
+const Dev = () => {
+    return (
+        <div className="dev-container">
+            
+
+            <Cards />
+
+
+        </div>
+    )
+}
+
+export default Dev
